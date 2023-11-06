fd55207a06d7398d6126dfbaf73c47f1ceaf8853
PitonWuk <rzavadskyi777@gmail.com>
2023-11-06 14:04:30 +0200

    task 1

--- task_01.py
+++ task_01.py
@@ -4,3 +4,33 @@
 дату народження, контактний телефон, місто, країну,
 домашню адресу. Реалізуйте методи класу для введення-виведення даних та інших операцій.
 """
+class Person:
+    def __init__(self, name, date, phone, city, country, address):
+        self.name = name
+        self.date = date
+        self.phone = phone
+        self.city = city
+        self.country = country
+        self.address = address
+
+    def display_info(self):
+        print(f"Name: {self.name}")
+        print(f"Date: {self.date}")
+        print(f"Phone: {self.phone}")
+        print(f"City: {self.city}")
+        print(f"Country: {self.country}")
+        print(f"Address: {self.address}")
+
+    def change_number(self, new_number):
+        self.phone = new_number
+        print(f"The number was updated to: {new_number}")
+
+person = Person("Makr Seli", "02.03.1999", "+385232125236", "Kyiv", "Ukraine", "Sobornosty, 22")
+person.display_info()
+
+person.change_number("+36251244852")
+person.display_info()
+
+
+
+
