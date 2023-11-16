58f59d072ef7c8a100b9bf6bc608bdc3ab5e9d25
PitonWuk <rzavadskyi777@gmail.com>
2023-11-16 22:51:42 +0200

    task 3, 4

--- task_03.py
+++ task_03.py
@@ -6,23 +6,57 @@
 Створіть для кожного класу необхідні методи та поля.
 """
 class Animal:
-    def __init__(self, name, species, sound):
+    def __init__(self, name, species):
         self.name = name
         self.species = species
-        self.sound = sound
 
-    def make_sound(self):
-        print(f"{self.species} {self.name} make sound {self.sound}")
-
-    def move(self):
-        print(f"{self.species} {self.name} move")
+    def display_info(self):
+        print(f"Name: {self.name}\nSpecies: {self.species}")
 
 class Tiger(Animal):
-    def __init__(self, name):
-        super().__init__(name, "Tiger", "Roar")
+    def __init__(self, name, color):
+        super().__init__(name, species="Tiger")
+        self.color = color
 
-    def swim(self):
-        super().swim()
+    def make_sound(self):
+        print("Roar!")
 
-tiged = Tiger("Sherhan")
-tiged.swim()
+    def display_info(self):
+        super().display_info()
+        print(f"Color: {self.color}")
+
+class Crocodile(Animal):
+    def __init__(self, name, size):
+        super().__init__(name, species="Crocodile")
+        self.size = size
+
+    def make_sound(self):
+        print("Grunt!")
+
+    def display_info(self):
+        super().display_info()
+        print(f"Size: {self.size}")
+
+class Kangaroo(Animal):
+    def __init__(self, name, jump_height):
+        super().__init__(name, species="Kangaroo")
+        self.jump_height = jump_height
+
+    def make_sound(self):
+        print("Boing!")
+
+    def display_info(self):
+        super().display_info()
+        print(f"Jump Height: {self.jump_height}")
+
+
+tiger = Tiger(name="Tiger", color="Orange")
+crocodile = Crocodile(name="Crocodile", size="Large")
+kangaroo = Kangaroo(name="Kangaroo", jump_height="High")
+
+animals = [tiger, crocodile, kangaroo]
+
+for animal in animals:
+    animal.display_info()
+    animal.make_sound()
+    print("\n")
--- task_04.py
+++ task_04.py
@@ -12,18 +12,30 @@
 поточний час за допомогою методу display_time.
 
 """
+from datetime import datetime
+
 class Clock:
     def __init__(self, hours, minutes):
         self.hours = hours
         self.minutes = minutes
 
-    def display_times(self):
-        super().display_times()
-        #print(f"{self.hours:02d}:{self.minutes:02d}")
-
-
 class AnalogClock(Clock):
-    pass
+    def display_time(self):
+        print(f"{self.hours:02d} hours and {self.minutes:02d} minutes")
 
-analog_clock = AnalogClock(1, 45)
-analog_clock.display_times()
+class DigitalClock(Clock):
+    def display_time(self):
+        print(f"{self.hours:02d}:{self.minutes:02d}")
+
+now = datetime.now()
+current_hours = now.hour
+current_minutes = now.minute
+
+analog_clock = AnalogClock(hours=current_hours, minutes=current_minutes)
+digital_clock = DigitalClock(hours=current_hours, minutes=current_minutes)
+
+print("Analog Clock:")
+analog_clock.display_time()
+
+print("\nDigital Clock:")
+digital_clock.display_time()
