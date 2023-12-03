c3827c08e0d6951f2b529e58550e72d5e27b4a4d
PitonWuk <rzavadskyi777@gmail.com>
2023-12-03 20:53:34 +0200

    task 1

--- lab_01.py
+++ lab_01.py
@@ -8,3 +8,38 @@
 зарахувалися тільки при виконанні певних умов
 (наприклад, якщо гроші на рахунку є).
 """
+class BankAccount:
+    def __init__(self, balance=0):
+        self._balance = balance
+
+    @property
+    def balance(self):
+        return self._balance
+
+    @balance.setter
+    def balance(self, amount):
+        if amount < 0:
+            print("The amount must be positive.")
+        else:
+            self._balance = amount
+            print(f"Balance updated: {amount}")
+
+    def deposit(self, amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        if amount > self.balance:
+            print("Not enough money")
+        else:
+            self.balance -= amount
+            print(f"Withdrawn {amount} . Balance: {self.balance}")
+
+
+account = BankAccount(1000)
+print(f"Opening balance sheet: {account.balance}.")
+
+account.withdraw(500)
+account.deposit(200)
+account.balance = 1500
+
+print(f"Final balance sheet: {account.balance}.")
