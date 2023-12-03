acff3bb32076d0a57d43b5ba3711b972fe384a07
PitonWuk <rzavadskyi777@gmail.com>
2023-12-03 21:57:41 +0200

    task 1

--- task_01.py
+++ task_01.py
@@ -1,55 +1,93 @@
 """
-Завдання 1
-Створіть базовий клас «Фігура» з методом для підрахунку
-площі. Створіть похідні класи: прямокутник, коло, прямокутний трикутник, трапеція, зі своїми методами для підрахунку
-площі.
+Завдання
+Користувач вводить з клавіатури набір чисел. Отримані
+числа необхідно зберегти до списку (тип списку оберіть в залежності від поставленого нижче завдання).
+Після чого покажіть меню, в якому запропонуєте користувачеві набір пунктів:
+1. Додати нове число до списку (якщо таке число існує у
+списку, потрібно вивести повідомлення про це користувачеві без додавання числа).
+2. Видалити усі входження числа зі списку (користувач вводить з клавіатури число для видалення)
+3. Показати вміст списку (залежно від вибору користувача,
+покажіть список з початку або з кінця)
+4. Перевірити, чи є значення у списку
+5. Замінити значення у списку (користувач визначає, чи
+замінити тільки перше входження, чи всі)
+Дія виконується залежно від вибору користувача, після
+чого меню з’являється знову
 """
-import math
-from abc import ABC, abstractmethod
+class NumberList:
+    def __init__(self):
+        self.numbers = []
 
-class Figure(ABC):
-    @abstractmethod
-    def area(self):
-        pass
+    def add_number(self, number):
+        if number not in self.numbers:
+            self.numbers.append(number)
+            print(f"Number {number} added to the list.")
+        else:
+            print(f"Number {number} already in the list.")
 
-class Rectangle(Figure):
-    def __init__(self, length, width):
-        self.length = length
-        self.width = width
+    def remove_number(self, number):
+        if number in self.numbers:
+            self.numbers.remove(number)
+            print(f"Number {number} removed from the list.")
+        else:
+            print(f"Number {number} not in the list.")
 
-    def area(self):
-        return self.length * self.width
+    def show_list(self, reverse=False):
+        if reverse:
+            print("List contents (from the end):", self.numbers[::-1])
+        else:
+            print("List contents (from the beginning):", self.numbers)
 
-class Circle(Figure):
-    def __init__(self, radius):
-        self.radius = radius
+    def check_number(self, number):
+        if number in self.numbers:
+            print(f"Number {number} is in the list.")
+        else:
+            print(f"Number {number} is not in the list.")
 
-    def area(self):
-        return math.pi * self.radius**2
+    def replace_number(self, old_number, new_number, replace_all=False):
+        if old_number in self.numbers:
+            if replace_all:
+                self.numbers = [new_number if num == old_number else num for num in self.numbers]
+            else:
+                index = self.numbers.index(old_number)
+                self.numbers[index] = new_number
 
-class Triangle(Figure):
-    def __init__(self, base, height):
-        self.base = base
-        self.height = height
+            print(f"Number {old_number} replaced by {new_number}.")
+        else:
+            print(f"Number {old_number} is not in the list.")
 
-    def area(self):
-        return 0.5 * self.base * self.height
+number_list = NumberList()
 
-class Trapezoid(Figure):
-    def __init__(self, base1, base2, height):
-        self.base1 = base1
-        self.base2 = base2
-        self.height = height
+while True:
+    print("\nMenu:")
+    print("1. Add a new number to the list")
+    print("2. Remove all occurrences of a number from the list")
+    print("3. Show list contents")
+    print("4. Check if the value is in the list")
+    print("5. Replace a value in a list")
+    print("0. Exit the program")
 
-    def area(self):
-        return 0.5 * (self.base1 + self.base2) * self.height
+    choice = input("Select the option: ")
 
-rectangle = Rectangle(5, 8)
-circle = Circle(4)
-triangle = Triangle(6, 10)
-trapezoid = Trapezoid(3, 7, 4)
-
-figures = [rectangle, circle, triangle, trapezoid]
-
-for figure in figures:
-    print(f"Area of {type(figure).__name__}:  {figure.area()}")
+    if choice == "1":
+        number = int(input("Enter the number to add: "))
+        number_list.add_number(number)
+    elif choice == "2":
+        number = int(input("Enter the number to delete: "))
+        number_list.remove_number(number)
+    elif choice == "3":
+        reverse_order = input("Print the list in reverse order? (y/n): ").lower() == 'y'
+        number_list.show_list(reverse=reverse_order)
+    elif choice == "4":
+        number = int(input("Enter the number to check: "))
+        number_list.check_number(number)
+    elif choice == "5":
+        old_number = int(input("Enter the number to replace: "))
+        new_number = int(input("Enter a new number: "))
+        replace_all = input("Replace all occurrences? (y/n): ").lower() == 'y'
+        number_list.replace_number(old_number, new_number, replace_all)
+    elif choice == "0":
+        print("The program is completed.")
+        break
+    else:
+        print("Wrong choice. Please try again.")
