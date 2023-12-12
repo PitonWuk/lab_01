c1b91795fd65188c128954a0cbe9b464ee2d8d7d
PitonWuk <rzavadskyi777@gmail.com>
2023-12-12 20:37:37 +0200

    task 1

--- task_02.py
+++ task_02.py
@@ -5,3 +5,29 @@
 метаклас.
 
 """
+class RequiredAttributesMeta(type):
+    def __init__(cls, name, bases, dct):
+        required_attributes = getattr(cls, 'required_attributes', [])
+
+        for attribute in required_attributes:
+            if attribute not in dct:
+                raise AttributeError(f"Class {cls.__name__} should be attribute '{attribute}'.")
+
+        super().__init__(name, bases, dct)
+
+class MyClass1(metaclass=RequiredAttributesMeta):
+    required_attributes = ['attr1', 'attr2']
+
+    def __init__(self, attr1, attr2):
+        self.attr1 = attr1
+        self.attr2 = attr2
+
+class MyClass2(metaclass=RequiredAttributesMeta):
+    required_attributes = ['attr1', 'attr3']
+
+    def __init__(self, attr1, attr3):
+        self.attr1 = attr1
+        self.attr3 = attr3
+
+#obj1 = MyClass1(attr1='value1', attr2='value2')
+obj2 = MyClass2(attr1='value1', attr3='value3')
